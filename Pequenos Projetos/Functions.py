c515bcfa3f7ab85941ab0b11b00d3c7d4980c610
Colomk <41271648+Colomk@users.noreply.github.com>
2022-05-28 00:13:55 -0300

    Projects

--- Pequenos Projetos/Functions.py
+++ Pequenos Projetos/Functions.py
@@ -1,10 +1,24 @@
-class VideoCard:
-    def __init__(self, marca, modelo, price):
-        self.marca = marca
-        self.modelo = modelo
-        self.price = price
+class Cart:
+
+    def __init__(self) -> None:
+        self.purchases = {}  # carrinho de compras
+
+    def insect_products(self, compras, price):  # adiciona itens e preços no carrinho
+        if 'Cart Items' not in self.purchases:
+            self.purchases['Cart Items'] = {compras: 'R$'+price}
+        else:
+            self.purchases['Cart Items'].update({compras: 'R$'+price})
+
+    def list_cart(self):  # mostra os itens do carrinho enumerados
+        carrinho = pay.purchases['Cart Items']
+        for a, (b, c) in enumerate(carrinho.items()):
+            print(f'\033[34m Item Code {a} - {b} = R${c}\033[m')
+
+        print(
+            # itens do carrinho
+            f'O seu carrinho contem {len(pay.purchases["Cart Items"])}'
+            f'e ele tem um valor total de: R${total}')  # valor total do carrinho
 
 
-high_end = VideoCard('Rog Strix', 'RTX 3090ti', 'R$12000')
-mid_end = VideoCard('Asus', 'GTX 1050ti', 'R$1500')
-low_end = VideoCard('MSI', 'GT 730', 'R$450')
+total = 0  # valor total
+pay = Cart()  # Chama a classe Cart
---
+++ Pequenos Projetos/ecommerce-geek.py
@@ -0,0 +1,28 @@
+from Functions import pay  # importa o arquivo Function que contem as classes e funções
+print(f'{"Geek Commerce":-^40}')
+
+while True:
+    options = int(input('Seja bem-vindo a Geek Commerce'
+                        '\n Como podemos te ajudar? '
+                        '\n [ 1 ] - Produtos'
+                        '\n [ 2 ] - Ir para o carrinho'
+                        '\n [ 3 ] - Pagamento'
+                        '\n ...'))
+    match options:
+        case 1:
+            compras = input('Qual item voce deseja comprar? ').upper().strip()
+            price = (input('Qual o valor do produto? '))
+
+            print(
+                f'O item \033[35m"{compras}"\033[m foi adicionado ao seu carrinho de compras e tem o valor de \033[35mR${price}\033[m')
+            pay.insect_products(compras, price)
+        case 2:
+            if len(pay.purchases) == 0:
+                print('\033[31m O seu carrinho nao tem nenhum item!\033[m')
+            else:
+                pay.list_cart()
+
+        case 3:
+            payment = int(input('Escolha a forma de pagamento:'
+                                '\n [ 1 ] - A vista no debito/cartão ou Pix - 10% de desconto'
+                                '\n [ 2 ] - Cartão de credito - Juros a partir de 5x e 10x'))
