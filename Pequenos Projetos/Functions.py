f09d794085c52b63491c21e6a719479f99bc8cc6
Colomk <41271648+Colomk@users.noreply.github.com>
2022-06-04 10:59:29 -0300

    projects

--- Pequenos Projetos/Functions.py
+++ Pequenos Projetos/Functions.py
@@ -1,9 +1,10 @@
 class Cart:
-
-    def __init__(self) -> None:
+    def __init__(self):
         self.purchases = {}  # carrinho de compras
 
-    def insect_products(self, compras, price):  # adiciona itens e preços no carrinho
+    # adiciona itens e preços no carrinho
+
+    def insect_products(self, compras, price):
         if 'Cart Items' not in self.purchases:
             self.purchases['Cart Items'] = {compras: int(price)}
         else:
@@ -11,16 +12,73 @@ class Cart:
 
     def list_cart(self):  # mostra os itens do carrinho enumerados
         carrinho = self.purchases['Cart Items']
-        total = 0
-        total = sum(map(lambda
-                        carrinho: float(carrinho), carrinho.values()))
+
         for a, (b, c) in enumerate(carrinho.items()):
             print(f'\033[34m Item Code {a} - {b} = R${c}\033[m')
 
         print(
             # itens do carrinho
             f'O seu carrinho contem \033[32m{len(self.purchases["Cart Items"])}\033[m itens'
-            f' e ele tem um valor total de: \033[32mR${total}\033[m')  # valor total do carrinho
+            f' e ele tem um valor total de: \033[32mR${sum_total(self)}\033[m')  # valor total do carrinho
+
+    def payment(self):
+        payment = int(input('Escolha a forma de pagamento:'
+                            '\n [ 1 ] - A vista no debito/cartão ou Pix - 10% de desconto'
+                            '\n [ 2 ] - Cartão de credito - Juros a partir de 5x pra cima'
+                            '\n ...'))
+        match payment:
+            case 1:
+                money = int(input('Pagamento em dinheiro foi selecionado'
+                                  '\n Digite o valor que ira pagar: '))
+                if money < sum_total():
+                    print(
+                        'Voce nao tem dinheiro suficiente, selecione outra opção de pagamento')
+                    self.payment()
+
+                elif money == sum_total():
+                    print(
+                        '\033[34m O seu pagamento foi um sucesso, obrigado pro comprar na "Geek Commerce!\033[m')
+
+                elif money > sum_total(self):
+                    a = money - sum_total(self)
+                    print(
+                        f'voce nos deu {money:.2f} e seu troco e de {a}')
+                    print(
+                        '\033[34m O seu pagamento foi um sucesso, obrigado pro comprar na "Geek Commerce!\033[m')
+                else:
+                    print(
+                        '\033[34m O seu pagamento foi um sucesso, obrigado pro comprar na "Geek Commerce!\033[m')
+            case 2:
+                credit = int(input('Em quantas vezes voce quer parcela? '))
+                print(
+                    f'A sua compra tem um total de R${sum_total(self)} e sera parcelado em X{credit}')
+
+                if credit < 3:  # 5% de desconto
+                    print(
+                        f'Voce recebeu \033[35m 5% \033[m de desconto e agora sua compra tera um valor total de:'
+                        f'\033[35m R${sum_total(self)-(sum_total(self) * 5 / 100)} \033[m')
+                    print(
+                        '\033[34m O seu pagamento foi um sucesso, obrigado pro comprar na "Geek Commerce!\033[m')
+
+                elif credit >= 7:  # 5% de juros
+                    print(f'Sera cobrado \033[35m 5% \033[m de juros e agora sua compra tem o valor total de'
+                          f'\033[35m R${sum_total(self)+(sum_total(self) * 5 / 100 )} \033[m')
+                    print(
+                        '\033[34m O seu pagamento foi um sucesso, obrigado pro comprar na "Geek Commerce!\033[m')
+
+                elif credit > 10:  # 10% de juros
+                    print(f'Sera cobrado \033[35m 10% \033[m de juros e agora sua compra tem o valor total de'
+                          f'\033[35m R${sum_total(self)+(sum_total(self) * 10 / 100 )} \033[m')
+                    print(
+                        '\033[34m O seu pagamento foi um sucesso, obrigado pro comprar na "Geek Commerce"!\033[m')
+
+
+def sum_total(self):
+    carrinho = self.purchases['Cart Items']
+    total = 0
+    total = sum(map(lambda
+                    carrinho: float(carrinho), carrinho.values()))
+    return total
 
 
 pay = Cart()  # Chama a classe Cart
--- Pequenos Projetos/ecommerce-geek.py
+++ Pequenos Projetos/ecommerce-geek.py
@@ -1,31 +1,49 @@
 from Functions import pay  # importa o arquivo Function que contem as classes e funções
+from random import randint
+from datetime import datetime
+
 print(f'{"Geek Commerce":-^40}')
 
 while True:
-    options = int(input('Seja bem-vindo a Geek Commerce'
-                        '\n Como podemos te ajudar? '
-                        '\n [ 1 ] - Produtos'
-                        '\n [ 2 ] - Ir para o carrinho'
-                        '\n [ 3 ] - Pagamento'
-                        '\n ...'))
-    match options:
+    options1 = int(input('Seja bem-vindo a Geek Commerce'
+                         '\n Como podemos te ajudar? '
+                         '\n [ 1 ] - Comprar'
+                         '\n [ 2 ] - Ir para o carrinho'
+                         '\n [ 3 ] - Pagamento'
+                         '\n ...'))
+    match options1:
         case 1:
             compras = input('Qual item voce deseja comprar? ').upper().strip()
             price = int(input('Qual o valor do produto? '))
-
             print(
                 f'O item \033[35m"{compras}"\033[m foi adicionado ao seu carrinho de compras e tem o valor de \033[35mR${price}\033[m')
             pay.insect_products(compras, price)
+
         case 2:
             if len(pay.purchases) == 0:
                 print('\033[31m O seu carrinho nao tem nenhum item!\033[m')
             else:
                 pay.list_cart()
+                options2 = int(input('Oque voce deseja fazer agora?'
+                                     '\n [ 1 ] - pagamento'
+                                     '\n [ 2 ] - voltar'
+                                     '\n ...'))
+                if options2 == 1:
+                    pay.payment()
+                    break
+                elif options2 == 2:
+                    continue
 
         case 3:
             if len(pay.purchases) == 0:
                 print('\033[31m O seu carrinho nao tem nenhum item!\033[m')
             else:
-                payment = int(input('Escolha a forma de pagamento:'
-                                    '\n [ 1 ] - A vista no debito/cartão ou Pix - 10% de desconto'
-                                    '\n [ 2 ] - Cartão de credito - Juros a partir de 5x e 10x'))
+                pay.payment()
+                break
+
+
+with open('note.txt', 'w+')as file:  # emissão da nota fiscal
+    file.write(f'{"Geek Commerce":-^40}'
+               f'\n {"Cupom fiscal":-^40}'
+               f'\n CODE {randint(0,100)} - '  # Código gerado aleatoriamente
+               f'{datetime.today().strftime("%d/%m/%Y - %H:%M:%S")}')  # Data atual, hora atual
