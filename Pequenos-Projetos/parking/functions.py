6033197e0a36902da57364e77d9981baf3439675
Igor Freitas <41271648+igorfreits@users.noreply.github.com>
2022-07-09 11:38:57 -0300

    projects

--- Pequenos-Projetos/parking/functions.py
+++ Pequenos-Projetos/parking/functions.py
@@ -1,13 +1,6 @@
-from sklearn.ensemble import RandomForestRegressor
-import pandas as pd
-import matplotlib
-import seaborn as sns
-import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
 from data import lista_de_carros, c
 from time import sleep
-from sklearn import metrics
+
 
 
 class estacionamento:
@@ -99,44 +92,9 @@ class estacionamento:
         print('Todas as vagas foram liberadas!')
 
     def relatorio(self):
+        print('Finge que aparece varios graficos aqui')
 
-        tabela = pd.read_csv(
-            r'C:\Users\Igor\Desktop\Python\Dungeons-e-Codes\Hashtag Treinamentos\Base de dados hastag\advertising.csv')
-        print(tabela)
-        sns.heatmap(tabela.corr(), cmap='Wistia', annot=True)
-        plt.show()
-
-        y = tabela['Vendas']
-        x = tabela[['TV', 'Radio', 'Jornal']]
-
-        x_treino, x_teste, y_treino, y_teste = train_test_split(
-            x, y, test_size=0.3, random_state=1)
-
-        modelo_regressaolinear = LinearRegression()
-        modelo_arvorededecisao = RandomForestRegressor()
-
-        modelo_regressaolinear.fit(x_treino, y_treino)
-        modelo_arvorededecisao.fit(x_treino, y_treino)
-
-        previsao_regressaolinear = modelo_regressaolinear.predict(x_teste)
-        previsao_arvorededecisao = modelo_arvorededecisao.predict(x_teste)
-
-        print(metrics.r2_score(y_teste, previsao_arvorededecisao))
-        print(metrics.r2_score(y_teste, previsao_regressaolinear))
-
-        tabela_auxiliar = pd.DataFrame()
-        tabela_auxiliar['y_teste'] = y_teste
-        tabela_auxiliar['Previsao de Regresso Linear'] = previsao_regressaolinear
-        tabela_auxiliar['Previsao arvore de decisao'] = previsao_arvorededecisao
-
-        plt.figure(figsize=(15, 6))
-        sns.lineplot(data=tabela_auxiliar)
-        plt.show()
-
-        nova_tabela = pd.read_csv(
-            r'C:\Users\Igor\Desktop\Estudos\Python\Hashtag Treinamentos\Base de dados hastag\Novos.csv')
-        previsao = modelo_arvorededecisao.predict(nova_tabela)
-        print(previsao)
+        
 
 
 teste = estacionamento()
