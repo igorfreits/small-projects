1a4b1a49bec1a4753936bd89d2abebd197f13cad
Igor Freitas <41271648+igorfreits@users.noreply.github.com>
2022-07-13 12:21:23 -0300

    projects

--- Pequenos-Projetos/ecommerce-geek/a.py
+++
@@ -1,5 +0,0 @@
-a = {'a': 1, 'b': 2, 'c': 3}
-
-with open('a.txt', 'w+') as f:
-    for x, y in a.items():
-        f.write(f'R${x}..........{y}\n')
--- Pequenos-Projetos/item-registration/functions.py
+++
@@ -1,3 +0,0 @@
-class Register():
-    def __init__(self, id, product, provider, entry_date, quantity):
-        pass
