a9ac182f1dfed4e066a2c1b9e1a1c1c5ee7b04d5
Igor Freitas <41271648+igorfreits@users.noreply.github.com>
2022-08-08 22:55:49 -0300

    project inventory rpg

---
+++ inventory-rpg/menu.py
@@ -0,0 +1,17 @@
+from functions import inventory
+
+while True:
+    option = int(
+        input('\033[36m'
+              '[1] - New item'
+              '\n[2] - Show inventory'
+              '\n[3] - Drop item'
+              '\n[4] - Exit'
+              '\nchoose option: \033[m'))
+    match option:
+        case 1:
+            inventory.new_item()
+        case 2:
+            inventory.show_inventory()
+        case 4:
+            break
