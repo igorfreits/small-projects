7de7d86eed8c6922c7b717fee32d7d209ada7a6c
Igor Freitas <igorfreits13@gmail.com>
2025-02-08 15:56:11 -0300

    deleted:    Envio-automativo-kontik/.gitignore 	deleted:    Envio-automativo-kontik/Comandos SQL.txt 	deleted:    Envio-automativo-kontik/commands.sql 	deleted:    Envio-automativo-kontik/generate_files.py 	deleted:    Envio-automativo-kontik/menu.py 	deleted:    Envio-automativo-kontik/send_email_update.py 	deleted:    Pokedex/.vscode/settings.json 	deleted:    Pokedex/manage.py 	deleted:    Pokedex/pokedex/__init__.py 	deleted:    Pokedex/pokedex/asgi.py 	deleted:    Pokedex/pokedex/settings.py 	deleted:    Pokedex/pokedex/urls.py 	deleted:    Pokedex/pokedex/wsgi.py 	deleted:    Pokedex/pokemon/__init__.py 	deleted:    Pokedex/pokemon/admin.py 	deleted:    Pokedex/pokemon/apps.py 	deleted:    Pokedex/pokemon/migrations/__init__.py 	deleted:    Pokedex/pokemon/models.py 	deleted:    Pokedex/pokemon/templates/pokemon/pokedex.html 	deleted:    Pokedex/pokemon/templates/pokemon/pokemon.html 	deleted:    Pokedex/pokemon/tests.py 	deleted:    Pokedex/pokemon/urls.py 	deleted:    Pokedex/pokemon/utils.py 	deleted:    Pokedex/pokemon/views.py 	deleted:    Pokedex/templates/base.html 	deleted:    Pokedex/templates/parciais/_messages.html 	deleted:    Pokedex/templates/parciais/_nav.html 	deleted:    Pokedex/templates/static/css/style.css 	deleted:    Pokedex/templates/static/images/favicon.ico 	deleted:    Pokedex/templates/static/images/logo-pokemon.png 	deleted:    Pokedex/templates/static/images/pokedex-icon.png 	deleted:    Pokedex/templates/static/images/pokedex-img.png 	deleted:    analise-de-dados/Envio_email.py 	deleted:    analise-de-dados/analise_dados.py 	deleted:    analise-de-dados/analise_dados_Zupper.py 	deleted:    analise-de-dados/data/Parametros.xlsx 	deleted:    analise-de-dados/data/Processado Erro.xlsx 	deleted:    analise-de-dados/functions.py 	deleted:    analise-de-dados/info.py 	deleted:    analise-de-dados/mensagens_erro.py 	deleted:    cube-calculator/Calculadora de cubagem Tecnolandia.py 	deleted:    ecommerce-geek/functions.py 	deleted:    ecommerce-geek/gerk-commerce.py 	deleted:    ohms-calculator/Calculadora de ohms e watts.py 	deleted:    "report-submission/Automa\303\247\303\243o de Processos - envio de laudos.py" 	deleted:    report-submission/bin/chromedriver.exe

--- Pokedex/pokemon/urls.py
+++
@@ -1,9 +0,0 @@
-from . import views
-from django.urls import path
-
-app_name = 'pokemon'
-
-urlpatterns = [
-    path('', views.PokedexView.as_view(), name='pokedex'),
-    path('search/', views.PokemonView.as_view(), name='search'),
-]
